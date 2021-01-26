18a5640c3094fe8d6f3415f1a59c0becd019963a
NazShakib <nazmus.ewu@gmail.com>
2021-01-26 12:14:55 +0600

    Day 1 & Day 2 added

---
+++ Day_2/ClassPractice/PartTimeEmployee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice
+{
+    class PartTimeEmployee : Employee
+    {
+        string hourly;
+
+        //public override void printFullName()
+        //{
+        //    Console.WriteLine(Firstname+" "+Lastname+" -Part Time employee");
+        //}
+
+        public new void printFullName()
+        {
+            Console.WriteLine(Firstname + " " + Lastname + " -Part Time employee");
+        }
+
+        // comment-> cltr+k+c
+        // uncomiit-> cltr+k+u
+        // ctor -> constuctor
+        // prop-> getter/ setter
+
+
+    }
+}
