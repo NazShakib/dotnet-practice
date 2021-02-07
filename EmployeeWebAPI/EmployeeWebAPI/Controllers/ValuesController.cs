74b0b0b87b11f430f94a9a96327c7cd3363aed78
NazShakib <nazmus.ewu@gmail.com>
2021-02-07 19:15:51 +0600

    implemented API's Get method to conneted with DB

---
+++ EmployeeWebAPI/EmployeeWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIDemo.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        static List<string> lists = new List<string> {
+            "Value0","Value1","Value2"
+            };
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return lists;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return lists[id];
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+            lists.Add(value);
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+            lists[id] = value;
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            lists.RemoveAt(id);
+        }
+    }
+}
